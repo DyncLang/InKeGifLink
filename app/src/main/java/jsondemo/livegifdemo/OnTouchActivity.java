0c5209ea7f21b5b51d019ee04de7c2747a08e4b2
DyncLang <zhulang@dync.cc>
2016-09-08 21:23:48 +0800

    完成上下切换直播房间

--- app/src/main/java/jsondemo/livegifdemo/OnTouchActivity.java
+++ app/src/main/java/jsondemo/livegifdemo/OnTouchActivity.java
@@ -1,14 +1,44 @@
 package jsondemo.livegifdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import jsondemo.livegifdemo.wdiget.OnTouchChangeViewLayout;
 
 public class OnTouchActivity extends AppCompatActivity {
+
+    private ImageView imgTop;
+    private ImageView imgContent;
+    private ImageView imgBottom;
+    private OnTouchChangeViewLayout rootViwe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_touch);
+        imgTop = (ImageView) findViewById(R.id.img_top);
+        imgContent = (ImageView) findViewById(R.id.img_content);
+       // imgBottom = (ImageView) findViewById(R.id.img_bottom);
+        rootViwe = (OnTouchChangeViewLayout) findViewById(R.id.activity_on_touch);
+
+        rootViwe.setOnChangeViewListner(new OnTouchChangeViewLayout.OnChangeViewListener() {
+            @Override
+            public void onNextView() {
+                Toast.makeText(OnTouchActivity.this,"下一个视图",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onBackView() {
+                Toast.makeText(OnTouchActivity.this,"上一个视图",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onClickShow(View view) {
+        Toast.makeText(this,"nihoa",Toast.LENGTH_SHORT).show();
     }
 
 }
--- app/src/main/java/jsondemo/livegifdemo/wdiget/OnTouchChangeViewLayout.java
+++ app/src/main/java/jsondemo/livegifdemo/wdiget/OnTouchChangeViewLayout.java
@@ -1,98 +1,217 @@
 package jsondemo.livegifdemo.wdiget;
 
 import android.content.Context;
-import android.graphics.Point;
-import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Scroller;
 
 /**
  * Created by Xiao_Bailong on 2016/9/8 0008.
  */
 
-public class OnTouchChangeViewLayout extends FrameLayout {
+public class OnTouchChangeViewLayout extends ViewGroup {
 
-    String TAG = this.getClass().getSimpleName();
+    private View imgTop;
+    private View imgContent;
+    private View imgBottom;
 
-    private ViewDragHelper mDragger;
+    private Context mContext;
+    private int mScreenHeight;
+    private int mScreenWidth;
+    private Scroller mScroller = null;
 
-    private View mDragView;
-
-    private View mBrackView;
-
-
-    private Point mAutoBackOriginPos = new Point();
+    private int mMaxTop = 0;
+    private int mMeasuredHeight;
+    private int mDelayMillis = 700; //窗口切换动画
 
     public OnTouchChangeViewLayout(Context context) {
         super(context);
+        mContext = context;
+        init();
     }
+
 
     public OnTouchChangeViewLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mDragger = ViewDragHelper.create(this, 1.0f, new ViewDragHelper.Callback() {
-            @Override
-            public boolean tryCaptureView(View child, int pointerId) {
-                return true;
-            }
+        mContext = context;
+        init();
 
-            @Override
-            public int clampViewPositionHorizontal(View child, int left, int dx) {
-                final int leftBound = getPaddingLeft();
-                final int rightBound = getWidth() - mDragView.getWidth() - leftBound;
+    }
 
-                final int newLeft = Math.min(Math.max(left, leftBound), rightBound);
+    public OnTouchChangeViewLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mContext = context;
+        init();
+    }
 
-                return newLeft;
-            }
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-            @Override
-            public int clampViewPositionVertical(View child, int top, int dy) {
-                return top;
-            }
+        imgTop = getChildAt(0);
+        imgContent = getChildAt(1);
+        imgBottom = getChildAt(2);
 
-            //手指释放的时候回调
-            @Override
-            public void onViewReleased(View releasedChild, float xvel, float yvel) {
-                Log.e(TAG, "onViewReleased: " + releasedChild + "xvel" + xvel + "yvel" + yvel);
-                //mAutoBackView手指释放时可以自动回去
-                if (releasedChild == mBrackView) {
-                    mDragView.scrollTo(0, 0);
-
-                    invalidate();
-                }
-            }
-        });
+        imgTop.getLayoutParams().width = mScreenWidth;
+        imgTop.getLayoutParams().height = mScreenHeight;
+        imgContent.getLayoutParams().width = mScreenWidth;
+        imgContent.getLayoutParams().height = mScreenHeight;
+        imgBottom.getLayoutParams().width = mScreenWidth;
+        imgBottom.getLayoutParams().height = mScreenHeight;
+        measureChild(imgTop, widthMeasureSpec, heightMeasureSpec);
+        measureChild(imgContent, widthMeasureSpec, heightMeasureSpec);
+        measureChild(imgBottom, widthMeasureSpec, heightMeasureSpec);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        super.onLayout(changed, l, t, r, b);
-        Log.e(TAG, "onLayout: l--" + l + "t" + t + "r--" + r + "b--" + b);
+        Log.e(l + "", "onLayout: l---" + "t" + t + "r--" + r + "b" + b);
+        if (changed) {
+            imgTop.layout(0, -mScreenHeight, r, 0);
+            imgContent.layout(l, t, r, b);
+            imgBottom.layout(0, mScreenHeight, r, b + mScreenHeight);
+        }
 
-        mAutoBackOriginPos.x = mBrackView.getLeft();
-        mAutoBackOriginPos.y = mBrackView.getTop();
+        mMeasuredHeight = imgContent.getMeasuredHeight();
     }
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent event) {
-        return mDragger.shouldInterceptTouchEvent(event);
+    private void init() {
+        mScroller = new Scroller(mContext);
+
+        WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        mScreenHeight = outMetrics.heightPixels;
+        mScreenWidth = outMetrics.widthPixels;
     }
 
+
+    private float mLastMotionY = 0;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        mDragger.processTouchEvent(event);
+        float y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (mScroller != null) {
+                    if (!mScroller.isFinished()) {
+                        mScroller.abortAnimation();
+                    }
+                }
+
+                mLastMotionY = y;
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+
+                int detaY = (int) (mLastMotionY - y);
+
+                scrollTo(0, detaY);
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+
+                ScrollView(0, (int) y);
+
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+
+                break;
+        }
+
         return true;
     }
 
-    @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
 
-        mDragView = getChildAt(0);
-        mBrackView = getChildAt(1);
+    private void ScrollView(int velocityX, int velocityY) {
+
+        int scrollY = getScrollY();
+        int DY;
+
+
+        if (scrollY > 0) {
+
+            if (Math.abs(scrollY) > (mMeasuredHeight / 2)) {
+                DY = mScreenHeight - Math.abs(scrollY);
+                postOnChangeView(true);
+            } else {
+                DY = -scrollY;
+            }
+
+            mScroller.startScroll(0, scrollY, 0, DY, Math.abs(mDelayMillis));
+
+        } else {
+
+            if (Math.abs(scrollY) > (mMeasuredHeight / 2)) {
+                DY = mScreenHeight - Math.abs(scrollY);
+                postOnChangeView(false);
+            } else {
+                DY = scrollY;
+            }
+            mScroller.startScroll(0, scrollY, 0, -DY, Math.abs(1000));
+        }
+
+
+        invalidate();
     }
+
+    /**
+     * 回调下一个视图
+     *
+     * @param isNext true 下一个 false 上一个
+     */
+    public void postOnChangeView(final boolean isNext) {
+        if (mOnChangeViewListener == null) {
+            return;
+        }
+        postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                if (isNext) {
+                    mOnChangeViewListener.onNextView();
+                } else {
+                    mOnChangeViewListener.onBackView();
+                }
+            }
+        }, mDelayMillis);
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            postInvalidate();
+        }
+        super.computeScroll();
+    }
+
+    private OnChangeViewListener mOnChangeViewListener;
+
+    /**
+     * 切换布局监听
+     */
+    public interface OnChangeViewListener {
+        void onNextView();
+
+        void onBackView();
+    }
+
+    /**
+     * 设置 视图切换回调监听；
+     *
+     * @param onChangeViewListner
+     */
+    public void setOnChangeViewListner(OnChangeViewListener onChangeViewListner) {
+        mOnChangeViewListener = onChangeViewListner;
+    }
+
 }
+
